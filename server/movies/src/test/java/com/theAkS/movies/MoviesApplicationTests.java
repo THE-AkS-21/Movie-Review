4fd6b1541d7ff8111b34047980bed65430adbe4d
THE_AkS <ankit.s2117@gmail.com>
2025-04-17 14:56:00 +0530

    MovieAPI
    first commit

---
+++ server/movies/src/test/java/com/theAkS/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theAkS.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
