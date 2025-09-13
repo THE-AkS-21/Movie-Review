a125d0ab8c4aeb00a507a61dbb541336257997b5
Ankit <ankit.s2117@gmail.com>
2025-09-13 20:26:11 +0530

    UPDATE

--- server/movies/src/test/java/com/theAkS/movies/MoviesApplicationTests.java
+++ server/movies/src/test/java/com/theAkS/movies/MoviesApplicationTests.java
@@ -2,12 +2,14 @@ package com.theAkS.movies;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // This tells Spring to use your application-test.properties
 class MoviesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
