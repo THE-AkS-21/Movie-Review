b98e19b2e9919d7c56f822ac5b17f9b8d1cd63b1
Ankit <ankit.s2117@gmail.com>
2025-09-15 21:48:24 +0530

    UPDATE

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import  React from 'react';
+import  ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import { theme } from './theme';
-import App from './App';
 import './index.css';
+import App from './App.tsx';
+import { theme } from './theme.ts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
