b5013950ef46f763c479ed4abeecd2f7d6b20edf
Ankit <ankit.s2117@gmail.com>
2025-11-05 20:03:53 +0530

    UPDATE

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+// @ts-ignore
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// If you need path aliases, use ESM import:
+// import path from 'node:path'
+
 export default defineConfig({
     plugins: [react()],
+    // server: { host: true, port: 3000 },
+    build: { outDir: 'dist' }
+    // resolve: {
+    //   alias: { '@': path.resolve(__dirname, 'src') }
+    // }
 })
